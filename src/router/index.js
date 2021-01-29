24ce22ebb712d424af62913bc1af9bf23ec1ce72
yurilemos <56370419+yurilemos@users.noreply.github.com>
2021-01-29 02:00:03 -0300

    commit on 'master'

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -10,10 +13,17 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique ad mollitia temporibus repellat dolores architecto molestias est eius provident corporis officiis, quam adipisci, ipsam, fuga perspiciatis nulla distinctio harum praesentium totam tenetur neque non blanditiis! Blanditiis culpa quibusdam, mollitia deleniti nam nihil vel ipsam eos qui odit error rerum, voluptate laudantium quod minus praesentium, dolore iste. Doloribus, dignissimos? Quam placeat tempore ipsam, laborum, animi molestias nostrum doloribus iure eveniet temporibus rerum enim ex sunt quia minus nisi ullam vel. Minus unde quasi voluptatibus asperiores obcaecati! Earum placeat quo ullam atque debitis. Doloremque id animi repellendus cum possimus consequuntur ea repudiandae?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique ad mollitia temporibus repellat dolores architecto molestias est eius provident corporis officiis, quam adipisci, ipsam, fuga perspiciatis nulla distinctio harum praesentium totam tenetur neque non blanditiis! Blanditiis culpa quibusdam, mollitia deleniti nam nihil vel ipsam eos qui odit error rerum, voluptate laudantium quod minus praesentium, dolore iste. Doloribus, dignissimos? Quam placeat tempore ipsam, laborum, animi molestias nostrum doloribus iure eveniet temporibus rerum enim ex sunt quia minus nisi ullam vel. Minus unde quasi voluptatibus asperiores obcaecati! Earum placeat quo ullam atque debitis. Doloremque id animi repellendus cum possimus consequuntur ea repudiandae?</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
